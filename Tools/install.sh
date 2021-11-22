e98c533affdaa2b42a5290565cbd1bb2ca76823f
WikipediaBrown <WikipediaBrown@users.noreply.github.com>
2021-11-22 01:42:14 -0800

    added template script

---
+++ Tools/install.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+# Configuration
+XCODE_TEMPLATE_DIR=$HOME'/Library/Developer/Xcode/Templates/File Templates/napkin'
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Copy RIBs file templates into the local RIBs template directory
+xcodeTemplate () {
+  echo "==> Copying up RIBs Xcode file templates..."
+
+  if [ -d "$XCODE_TEMPLATE_DIR" ]; then
+    rm -R "$XCODE_TEMPLATE_DIR"
+  fi
+  mkdir -p "$XCODE_TEMPLATE_DIR"
+
+  cp -R $SCRIPT_DIR/*.xctemplate "$XCODE_TEMPLATE_DIR"
+  cp -R $SCRIPT_DIR/napkin.xctemplate/ownsView/* "$XCODE_TEMPLATE_DIR/napkin.xctemplate/ownsViewwithXIB/"
+  cp -R $SCRIPT_DIR/napkin.xctemplate/ownsView/* "$XCODE_TEMPLATE_DIR/napkin.xctemplate/ownsViewwithStoryboard/"
+}
+
+xcodeTemplate
+
+echo "==> ... success!"
+echo "==> napkins have been set up. In Xcode, select 'New File...' to use napkin templates."
