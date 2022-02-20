fe63c44192fcca1e6fff6028b5cdfa68ed7c3c33
WikipediaBrown <WikipediaBrown@users.noreply.github.com>
2022-02-20 01:14:14 -0800

    Final ci (#32)
    * updated
    
    * fixed stuff
    
    * another
    
    * added badges

--- Tools/napkin/Service Manager.xctemplate/Default/___FILEBASENAME___Manager.swift
+++ Tools/napkin/Service Manager.xctemplate/Default/___FILEBASENAME___Manager.swift
@@ -3,11 +3,11 @@
 import napkin
 
 /// The public API for the ___FILEBASENAME___Manager.
-protocol ___FILEBASENAME___Manager {
+protocol ___VARIABLE_productName___Managing {
     // TODO: Declare properties and methods needed to access the manager.
 }
 
-extension ___FILEBASENAME___Manager: ___VARIABLE_productName___Managing {
+class ___VARIABLE_productName___Manager: ___VARIABLE_productName___Managing {
 
-    // TODO: Implement properties to provide for ___VARIABLE_childName___ scope.
+    // TODO: Implement properties to provide for ___VARIABLE_productName___ scope.
 }
