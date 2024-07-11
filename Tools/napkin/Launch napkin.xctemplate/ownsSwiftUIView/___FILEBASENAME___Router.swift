4840809d6b237a21b1705d84199bd199943f5da0
WikipediaBrown <WikipediaBrown@users.noreply.github.com>
2024-07-10 22:27:37 -0700

    Fixed Launch Napkin with SwiftUI bug (#60)

--- Tools/napkin/Launch napkin.xctemplate/ownsSwiftUIView/___FILEBASENAME___Router.swift
+++ Tools/napkin/Launch napkin.xctemplate/ownsSwiftUIView/___FILEBASENAME___Router.swift
@@ -11,7 +11,7 @@ protocol ___VARIABLE_productName___ViewControllable: ViewControllable {
     // TODO: Declare methods the router invokes to manipulate the view hierarchy.
 }
 
-final class ___VARIABLE_productName___Router: ViewableRouter<___VARIABLE_productName___Interactable, ___VARIABLE_productName___ViewControllable>, ___VARIABLE_productName___Routing {
+final class ___VARIABLE_productName___Router: LaunchRouter<___VARIABLE_productName___Interactable, ___VARIABLE_productName___ViewControllable>, ___VARIABLE_productName___Routing {
 
     // TODO: Constructor inject child builder protocols to allow building children.
     override init(interactor: ___VARIABLE_productName___Interactable, viewController: ___VARIABLE_productName___ViewControllable) {
